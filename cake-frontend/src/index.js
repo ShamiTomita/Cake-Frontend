80d101157cb9f8495543438b6a0b02fc8140e61b
Shami <shamitomita@gmail.com>
2022-03-20 20:41:58 -0400

    removing unecessary code, adding folders

--- cake-frontend/src/index.js
+++ cake-frontend/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from "redux-thunk"
+import combineReducers from './reducers/index.js'
 import * as serviceWorker from './serviceWorker';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store = createStore(combineReducers, composeEnhancers(applyMiddleware(thunk)))
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
