e17cd8eed7b22a77afcb6f1b6b606c51efca1343
Shami <shamitomita@gmail.com>
2022-03-22 22:41:53 -0400

    fixing routes

--- cake-frontend/src/App.js
+++ cake-frontend/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/NavBar';
+import Home from "./containers/Home"
+import NavBar from './components/NavBar'
 import Header from './components/Header'
 import {Route, Routes} from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
+    <NavBar />
+    <Routes>
+      <Route path="/" elements={<Home/>}/>
+      <Route path="/browse"/>
+    </Routes>
       <div>
         <Header/>
-        <p>helloooooo</p>
+        <Home/>
       </div>
     </div>
   );
