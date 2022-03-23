bf8ca2611978600ab2e0ace9836c80c64d6bb3b7
Shami <shamitomita@gmail.com>
2022-03-22 22:41:11 -0400

    router fix

--- cake-frontend/src/index.js
+++ cake-frontend/src/index.js
@@ -6,13 +6,17 @@ import { Provider } from 'react-redux';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from "redux-thunk"
 import combineReducers from './reducers/index.js'
-
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let store = createStore(combineReducers, composeEnhancers(applyMiddleware(thunk)))
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+    <Router>
       <App />
+    </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
