2dc1b8b231fe87bdab23eed7e411c567c9edcb01
Shami <shamitomita@gmail.com>
2022-03-28 23:05:50 -0400

    header content

--- cake-frontend/src/components/Header.js
+++ cake-frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 const Header = (props) => {
   return (
     <div>
-      <h1>Im the Header</h1>
+      <h1>✧･ﾟ: *✧･ﾟ:* ₓ˚. ୭ ˚○◦˚.˚◦○˚ ୧ .˚ₓ *:･ﾟ✧*:･ﾟ✧</h1>
     </div>
   )
 }
