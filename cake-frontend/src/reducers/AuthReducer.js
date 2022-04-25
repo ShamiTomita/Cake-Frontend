5642d890d59b74b30451d20564f2ec5fd0b11512
ShamiTomita <shamitomita@gmail.com>
2022-04-25 15:36:37 -0400

    node updates

---
+++ cake-frontend/src/reducers/AuthReducer.js
@@ -0,0 +1,23 @@
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  authentication_token: '',
+  username: '',
+  errorFlag: false,
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'EMAIL_CHANGED':
+      return { ...state, email: action.payload };
+    case 'PASSWORD_CHANGED':
+      return { ...state, password: action.payload };
+    case 'LOGIN_FAILED':
+      return { ...state, errorFlag: true, password: '', spinner: false };
+    case 'LOGIN_USER_SUCCESS':
+      return { ...state, ...action.payload, ...INITIAL_STATE };
+    default:
+      return state;
+  }
+};
